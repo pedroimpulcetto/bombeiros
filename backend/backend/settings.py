9cc9bd041a783c45a59ecbba627c0b41019aeeaa
pedroimpulcetto <pedro.impulcetto@gmail.com>
2019-09-19 22:39:52 -0300

    host

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['erp-bombeiros.herokuapp.com']
+ALLOWED_HOSTS = ['erp-bombeiros.herokuapp.com', '127.0.0.1:8000']
 
 
 # Application definition
